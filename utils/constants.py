53cda7f62da01f39bde4b718ed926e1de33eb177
disrael <disrael@uclusion.com>
2019-07-02 08:23:34 -0700

    fix: remove investibles service

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,4 @@
-rest_api_backend_repos = ['uclusion_async', 'uclusion_investibles', 'uclusion_investible_api', 'uclusion_markets',
+rest_api_backend_repos = ['uclusion_async', 'uclusion_investible_api', 'uclusion_markets',
                           'uclusion_market_api', 'uclusion_shared_resources', 'uclusion_sso', 'uclusion_team_api',
                           'uclusion_users', 'uclusion_user_api', 'uclusion_webhooks', 'uclusion_websockets',
                           'uclusion_common', 'uclusion_backend_common', 'uclusion_summaries',
