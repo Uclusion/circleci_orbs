e360fe691d131efeae27f7fe4a6ace4282e6399b
disrael <disrael@uclusion.com>
2019-07-24 11:59:54 -0700

    fix: remove team api

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,4 @@
 rest_api_backend_repos = ['uclusion_async', 'uclusion_investible_api', 'uclusion_markets',
-                          'uclusion_market_api', 'uclusion_shared_resources', 'uclusion_sso', 'uclusion_team_api',
-                          'uclusion_users', 'uclusion_user_api', 'uclusion_webhooks', 'uclusion_websockets',
-                          'uclusion_common', 'uclusion_backend_common', 'uclusion_summaries',
-                          'common_lambda_dependencies']
+                          'uclusion_market_api', 'uclusion_shared_resources', 'uclusion_sso', 'uclusion_users',
+                          'uclusion_user_api', 'uclusion_webhooks', 'uclusion_websockets', 'uclusion_common',
+                          'uclusion_backend_common', 'uclusion_summaries', 'common_lambda_dependencies']
