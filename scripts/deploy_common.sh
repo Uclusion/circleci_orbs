cabeb7903e710944f4ea04320492d3d278ca3afb
disrael <disrael@gmail.com>
2020-06-21 14:39:55 -0700

    fix: global install of serverless was failing so try local

--- scripts/deploy_common.sh
+++ scripts/deploy_common.sh
@@ -17,7 +17,7 @@ function build_common() {
         PY_DIR='build/python/lib/python3.7/site-packages'
         mkdir -p $PY_DIR
         pip install --no-deps . -t $PY_DIR
-        /home/circleci/node_modules/serverless/bin/serverless.js deploy
+        /home/circleci/repo/node_modules/serverless/bin/serverless.js deploy
         echo build
     else
         echo skip
