f4aea8c472517c0f97ef7f0cf5d4b41fb4791f1c
disrael <disrael@gmail.com>
2023-02-03 13:55:23 -0800

    fix: fix cleanup release script

--- scripts/test_user_deleter.rb
+++ scripts/test_user_deleter.rb
@@ -5,19 +5,26 @@ require 'json'
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: test_user_deleter.rb --poolId=<yourpoolid>"
+  opts.banner = "Usage: test_user_deleter.rb --poolId=<yourpoolid> --all"
   opts.on("-p", "--poolId POOLID", "POOLID is required") do |p|
     options[:poolId] = p
   end
+  options[:all] = false
+  opts.on("-a", "--all", "all is false by default") do
+    options[:all] = true
+  end
 end.parse!
-
 
 def is_test_user(email)
   return email.start_with?('tuser') && email.end_with?('@uclusion.com')
 end
 
+def is_not_integration_user(email)
+  return !['david.israel@uclude.com', '827hooshang@gmail.com'].include?(email)
+end
 
 poolId = options[:poolId]
+doAll = options[:all]
 
 users_response_string = `aws cognito-idp list-users --user-pool-id=#{poolId}`
 users_response = JSON.parse(users_response_string)
@@ -28,8 +35,11 @@ users.each do |user|
   id = user['Username']
   email_attr = attributes.find {|attr| attr['Name'] == 'email'}
   email = email_attr['Value']
-#  puts(email)
-  isTest = is_test_user(email)
+  if doAll then
+    isTest = is_not_integration_user(email)
+  else
+    isTest = is_test_user(email)
+  end
   if isTest then
     puts("Deleting user #{email} with user name #{id}")
     command = "aws cognito-idp admin-delete-user --user-pool-id=#{poolId} --username=#{id}"
