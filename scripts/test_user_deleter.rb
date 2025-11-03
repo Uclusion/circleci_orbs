26708261fce42b1c1bd443a74ebf9d8ace80634f
David Israel <disrael@uclusion.com>
2025-11-02 17:16:08 -0800

    feat: github actions

--- scripts/test_user_deleter.rb
+++ scripts/test_user_deleter.rb
@@ -25,7 +25,6 @@ end
 
 poolId = options[:poolId]
 emails = options[:emails]
-puts("Listing users with pool is dev = #{poolId == 'us-west-2_DF7pMdI6r'}")
 users_response_string = `aws cognito-idp list-users --user-pool-id=#{poolId}`
 users_response = JSON.parse(users_response_string)
 users = users_response['Users']
